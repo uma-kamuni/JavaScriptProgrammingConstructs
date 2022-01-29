7cb000ac48c21e472548cf5feef14a16fe00e0f7
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-28 22:09:53 -0800

    Added: Add Two Random Dice Numbers

---
+++ SequenceProblems/Q3AddTwoDiceNumbers.js
@@ -0,0 +1,6 @@
+let number1 = Math.floor( Math.random() * 6 ) +1;
+console.log(number1);
+let number2 = Math.floor( Math.random() * 6 ) +1;
+console.log(number2);
+let add = number1 + number2;
+console.log(add);
