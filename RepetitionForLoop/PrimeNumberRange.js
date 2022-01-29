874ee570b06185f0cbb6d4e3e0aa237e6ccb8290
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-29 00:34:56 -0800

    Added: Check the given number is prime or not in the given range

---
+++ RepetitionForLoop/PrimeNumberRange.js
@@ -0,0 +1,24 @@
+ const prompt = require('prompt-sync')({sigint : true});
+
+ const lowerNumber = prompt('Enter lower number: ')
+ const higherNumber = prompt('Enter higher number: ')
+ 
+console.log("Prime numbers between entered range are: ")
+ 
+ // looping from lowerNumber to higherNumber
+ for (let i = lowerNumber; i <= higherNumber; i++) {
+     let flag = 0;
+ 
+     // looping through 2 to user input number
+     for (let j = 2; j < i; j++) {
+         if (i % j == 0) {
+             flag = 1;
+             break;
+         }
+     }
+ 
+     // if number greater than 1 and not divisible by other numbers
+     if (i > 1 && flag == 0) {
+         console.log(i);
+     }
+ }
