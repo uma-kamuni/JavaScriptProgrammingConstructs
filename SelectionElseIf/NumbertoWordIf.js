205d818dfa088bc0b83c49e6d0ce6937923c93a5
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-28 22:43:44 -0800

    Added: Take Single Digit Number and Read number in Word

---
+++ SelectionElseIf/NumbertoWordIf.js
@@ -0,0 +1,35 @@
+const prompt = require('prompt-sync')();
+const num = prompt(" Enter any number ");
+if(num == 0){
+    console.log("Zero");
+}
+else if(num == 1){
+    console.log("One");
+}
+else if(num == 2){
+    console.log("Two");
+}
+else if(num == 3){
+    console.log("three");
+}
+else if(num == 4){
+    console.log("four");
+}
+else if(num == 5){
+    console.log("five");
+}
+else if(num == 6){
+    console.log("Six");
+}
+else if(num == 7){
+    console.log("Seven");
+}
+else if(num == 8){
+    console.log("Eight");
+}
+else if(num == 9){
+    console.log("nine");
+}
+else{
+    console.log("Wrong input");
+}
